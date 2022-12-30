b6f7160fd16a0154af2239eb955f9e60d7ea3009
viettrinh1617 <79010025+viettrinh1617@users.noreply.github.com>
2022-12-30 19:27:57 +0700

    ui fix

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,13 @@
 import { Link as RouterLink } from "react-router-dom";
 import { Box } from "@mui/material";
 import logoImg from "../logo.png";
+import { ReactComponent as LogoSvg } from '../tmdb.svg';
 
 function Logo({ disabledLink = false, sx }) {
   const logo = (
-    <Box sx={{ width: 40, height: 40, ...sx }}>
-      <img src={logoImg} alt="logo" width="100%" />
+    <Box sx={{display:'flex', justifyContent: 'center', alignContent: 'center', width: 40, height: 40, ...sx }}>
+      {/* <img src={logoImg} alt="logo" width="100%" /> */}
+      <LogoSvg alt="logo" width="100%" />
     </Box>
   );
 
